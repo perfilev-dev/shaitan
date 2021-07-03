0acba2ec6ecf3c2385730fbb34cfb8f357834c36
perfilev <perfilev@perfilev>
2021-07-03 21:12:02 +0300

    Implement JSON request sending

--- src/discover/src/main.rs
+++ src/discover/src/main.rs
@@ -1,13 +1,9 @@
 use reflection::server_reflection_client::ServerReflectionClient;
 use reflection::server_reflection_request::MessageRequest;
-use reflection::{ServerReflectionRequest, ServerReflectionResponse};
+use reflection::ServerReflectionRequest;
 use reflection::server_reflection_response::MessageResponse;
-use tokio::time;
-use std::time::Duration;
-use protobuf::descriptor::{FileDescriptorProto, FileDescriptorSet};
-use protobuf::{Message, MessageDyn};
-use prost_types::{DescriptorProto, Method};
-use serde_protobuf::{descriptor, de};
+use protobuf::descriptor::{FileDescriptorProto, MethodDescriptorProto};
+use protobuf::Message;
 use protobuf::reflect::{FileDescriptor, MessageDescriptor};
 use protobuf::json::{parse_dynamic_from_str, print_to_string};
 use tonic::codegen::http;
@@ -17,15 +13,20 @@ use prost::DecodeError;
 use prost::encoding::{WireType, DecodeContext};
 use tonic::codec::ProstCodec;
 use futures::channel::mpsc;
-use futures::{StreamExt, SinkExt};
+use futures::SinkExt;
 use std::error::Error;
-use tonic::client::Grpc;
-use tonic::transport::Channel;
 use std::fmt::Formatter;
-use std::collections::{HashMap, HashSet};
-use std::collections::hash_map::Keys;
+use std::collections::HashMap;
 use protobuf::descriptor::field_descriptor_proto::Type;
-use serde::{Serialize, Deserialize};
+use serde::Serialize;
+use serde_json::{Value, json};
+use regex::Regex;
+use std::str::FromStr;
+
+lazy_static::lazy_static! {
+    // captures this: /helloworld.Greeter/SayHello
+    static ref PATH_RE: Regex = Regex::new(r"^/(?P<service>.+?)/(?P<method>.+?)$").unwrap();
+}
 
 
 pub mod reflection {
@@ -33,21 +34,20 @@ pub mod reflection {
 }
 
 #[derive(Debug, Default)]
-pub struct Message1 {
+pub struct RawBytes {
     pub bytes: Vec<u8>
 }
 
-impl prost::Message for Message1 {
+impl prost::Message for RawBytes {
     fn encode_raw<B>(&self, buf: &mut B) where
         B: BufMut,
         Self: Sized {
         buf.put(&*self.bytes);
     }
 
-    fn merge_field<B>(&mut self, tag: u32, wire_type: WireType, buf: &mut B, ctx: DecodeContext) -> Result<(), DecodeError> where
+    fn merge_field<B>(&mut self, _: u32, _: WireType, buf: &mut B, _: DecodeContext) -> Result<(), DecodeError> where
         B: Buf,
         Self: Sized {
-        println!("{}", buf.remaining());
         while buf.has_remaining() {
             self.bytes.push(buf.get_u8());
         }
@@ -83,22 +83,23 @@ impl std::error::Error for MyError {
 
 #[derive(Debug)]
 struct Executor {
-    client: ServerReflectionClient<Channel>,
+    address: String,
     protos: HashMap<String, FileDescriptorProto>
 }
 
 impl Executor {
 
-    async fn connect(addr: String) -> Result<Self, Box<dyn Error>> {
+    async fn connect(address: String) -> Result<Self, Box<dyn Error>> {
         let mut executor = Self {
-            client: ServerReflectionClient::connect(addr).await?,
+            address: address.to_string(),
             protos: Default::default()
         };
 
         // load protos!
+        let mut client = ServerReflectionClient::connect(address).await?;
         let (mut tx, rx) = mpsc::unbounded::<ServerReflectionRequest>();
 
-        let response = executor.client.server_reflection_info(tonic::Request::new(rx)).await?;
+        let response = client.server_reflection_info(tonic::Request::new(rx)).await?;
         let mut inbound = response.into_inner();
 
         // send list services
@@ -284,6 +285,77 @@ impl Executor {
         Ok(info)
     }
 
+    fn get_method(&self, service_name: &str, method_name: &str) -> Result<&MethodDescriptorProto, MyError> {
+        let proto = self.protos.get(service_name)
+            .ok_or(MyError::Other("service not found".to_string()))?;
+
+        for srv in &proto.service {
+            if service_name.ends_with(srv.get_name()) {
+                for method in &srv.method {
+                    if method.get_name() == method_name {
+                        return Ok(method)
+                    }
+                }
+            }
+        }
+
+        Err(MyError::Other("method not found".to_string()))
+    }
+
+    fn get_message_by_type_name(&self, name: &str) -> Option<MessageDescriptor> {
+        for proto in self.protos.values() {
+            let fd = FileDescriptor::new_dynamic(proto.clone(), vec![]);
+
+            if let Some(message) = fd.message_by_full_name(name) {
+                return Some(message);
+            }
+        }
+
+        None
+    }
+
+    async fn unary_json(&self, path: &str, value: &Value) -> Result<Value, Box<dyn Error>> {
+        let json_str = serde_json::to_string(value)?;
+
+        // parse request
+        let caps = PATH_RE.captures(path)
+            .ok_or(MyError::Other("wrong service path".to_string()))?;
+
+        // find method and types (input, output)
+        let method = self.get_method(&caps["service"], &caps["method"])?;
+        let input_type = self.get_message_by_type_name(method.get_input_type())
+            .ok_or(MyError::Other("input type not found!".to_string()))?;
+        let output_type = self.get_message_by_type_name(method.get_input_type())
+            .ok_or(MyError::Other("output type not found!".to_string()))?;
+
+        // create input message
+        let input = parse_dynamic_from_str(&input_type, &json_str)?;
+
+        let request: Request<RawBytes> = tonic::Request::new(RawBytes {
+            bytes: input.write_to_bytes_dyn()?
+        });
+
+        // connect!
+        let conn = tonic::transport::Endpoint::new(self.address.to_string())?.connect().await?;
+        let mut grpc = tonic::client::Grpc::new(conn);
+        grpc.ready().await?;
+
+        let codec: ProstCodec<RawBytes, RawBytes> = tonic::codec::ProstCodec::default();
+        let path = http::uri::PathAndQuery::from_str(path).unwrap();
+
+        // make request and got response...
+        let result: tonic::Response<RawBytes> = grpc.unary(request.into_request(), path, codec).await?;
+        let mut output = result.into_inner().bytes.clone();
+
+        // wtf is this? but it work!
+        output.insert(0, 10);
+
+        let mut out = output_type.new_instance();
+        out.merge_from_bytes_dyn(&output)?;
+
+        Ok(serde_json::from_str(&print_to_string(&*out).unwrap())?)
+    }
+
 }
 
 #[derive(Debug, Serialize)]
@@ -335,124 +407,16 @@ struct FieldTypeInfo {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let mut executor = Executor::connect("http://[::1]:50052".to_string()).await?;
+    let executor = Executor::connect("http://[::1]:50052".to_string()).await?;
 
     // print summary info about rpc services, enums, types...
     println!("INFO:\n{}", serde_json::to_string_pretty(&executor.info()?)?);
 
+    let result = executor.unary_json("/helloworld.Greeter/SayHello", &json!({
+        "name": "Sergey"
+    })).await?;
 
-    /*
-
-    // parse filedescriptorproto
-
-    let fdp_base64 = "ChBoZWxsb3dvcmxkLnByb3RvEgpoZWxsb3dvcmxkIiIKDEhlbGxvUmVxdWVzdBISCgRuYW1lGAEgASgJUgRuYW1lIiYKCkhlbGxvUmVwbHkSGAoHbWVzc2FnZRgBIAEoCVIHbWVzc2FnZTJHCgdHcmVldGVyEjwKCFNheUhlbGxvEhguaGVsbG93b3JsZC5IZWxsb1JlcXVlc3QaFi5oZWxsb3dvcmxkLkhlbGxvUmVwbHlKiQIKBhIEAAANAQoICgEMEgMAABIKCAoBAhIDAQATCgoKAgYAEgQDAAUBCgoKAwYAARIDAwgPCgsKBAYAAgASAwQCMwoMCgUGAAIAARIDBAYOCgwKBQYAAgACEgMEEBwKDAoFBgACAAMSAwQnMQoKCgIEABIEBwAJAQoKCgMEAAESAwcIFAoLCgQEAAIAEgMIAhIKDAoFBAACAAUSAwgCCAoMCgUEAAIAARIDCAkNCgwKBQQAAgADEgMIEBEKCgoCBAESBAsADQEKCgoDBAEBEgMLCBIKCwoEBAECABIDDAIVCgwKBQQBAgAFEgMMAggKDAoFBAECAAESAwwJEAoMCgUEAQIAAxIDDBMUYgZwcm90bzM=".to_string();
-    let fdp = base64::decode(fdp_base64).unwrap();
-
-    let t = FileDescriptorProto::parse_from_bytes(&fdp).unwrap();
-
-    for service in &t.service {
-        for method in &service.method {
-            println!("service: {}, method: {}, input: {}, output: {}", service.get_name(), method.get_name(), method.get_input_type(), method.get_output_type());
-        }
-    }
-
-    for t1 in &t.enum_type {
-        println!("enum: {:?}", t1);
-    }
-
-    for t2 in &t.message_type {
-        println!("messages: {:?}", t2);
-    }
-
-    // try to construct message
-
-    let fd = FileDescriptor::new_dynamic(t, vec![]);
-
-    // println!("{:?}", );
-
-    let m = fd.message_by_package_relative_name("HelloRequest").unwrap();
-    let mut msg = m.new_instance();
-
-    let tt1 = parse_dynamic_from_str(&m, r#"{"name": "sergeysergeysergeysergeysergeysergeysergeysergeysergeysergeysergeysergeysergeysergeysergeysergey"}"#).unwrap();
-
-    let export = tt1.write_to_bytes_dyn().unwrap();
-
-    println!("sent: {:?}", export);  // is it right???
-
-    let m1 = fd.message_by_package_relative_name("HelloReply").unwrap();
-    let mut msg1 = m.new_instance();
-
-    let tt2 = parse_dynamic_from_str(&m1, r#"{"message": "1ello sergeysergeysergeysergeysergeysergeysergeysergeysergeysergeysergeysergeysergeysergeysergeysergey!"}"#).unwrap();
-
-    let export2 = tt2.write_to_bytes_dyn().unwrap();
-
-    println!("should be: {:?}", export2);  // is it right???
-
-
-    // now we wan't to sent tt1 to server!
-
-    let r: Request<Message1> = tonic::Request::new(Message1 {
-        bytes: export
-    });
-
-    let mut conn = tonic::transport::Endpoint::new("http://[::1]:50052")?.connect().await?;
-    let mut grpc = tonic::client::Grpc::new(conn);
-
-    grpc.ready().await?;
-
-    let codec: ProstCodec<Message1, Message1> = tonic::codec::ProstCodec::default();
-    let path = http::uri::PathAndQuery::from_static("/helloworld.Greeter/SayHello");
-
-    let result: tonic::Response<Message1> = grpc.unary(r.into_request(), path, codec).await?;
-
-    //println!("result: {:?}", result.into_inner().bytes);
-
-    let mut output = result.into_inner().bytes.clone();
-    println!("actually: {:?}", output);
-
-    // wtf is this? but it work!
-    output.insert(0, 10);
-
-    let mut out = m1.new_instance();
-    let m = out.merge_from_bytes_dyn(&output);
-
-    println!("{}", print_to_string(&*out).unwrap());
-
-
-
-    // -----
-
-    let start = time::Instant::now();
-    let outbound = async_stream::stream! {
-        let mut interval = time::interval(Duration::from_secs(1));
-
-        while let time = interval.tick().await {
-            let elapsed = time.duration_since(start);
-            let note = ServerReflectionRequest {
-                host: "asd".to_string(),
-                message_request: Some(MessageRequest::FileByFilename("1".to_string()))
-            };
-
-            yield note;
-        }
-    };
-
-    let response = client.server_reflection_info(tonic::Request::new(outbound)).await?;
-    let mut inbound = response.into_inner();
-
-    while let Some(note) = inbound.message().await? {
-        println!("NOTE = {:?}", note);
-    }
-
-
-    let request = tonic::Request::new(ServerReflectionRequest {
-        host: "asd".to_string(),
-        message_request: Some(MessageRequest::ListServices("".to_string()))
-    });
-
-    let resp = client.server_reflection_info(request).await?;
-
-     */
+    println!("{:?}", result);
 
     Ok(())
 }
